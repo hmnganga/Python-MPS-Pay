de8a186dd28971796a1e017237fc3cf7a0d93825
678jupiter <jupiterhumphrey@gmail.com>
2022-01-28 17:03:45 +0300

    jjjaa

--- urls.py
+++ urls.py
@@ -21,10 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/payments/', include('mpesa.api.urls'))
+   
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
