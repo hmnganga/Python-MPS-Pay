f25b5e93d7eae93d41e5bef4458b472e299cf9bf
678jupiter <jupiterhumphrey@gmail.com>
2022-01-28 11:14:20 +0300

    pshavc

--- mpesa/api/views.py
+++ mpesa/api/views.py
@@ -3,11 +3,12 @@ from mpesa.api.serializers import LNMOnlineSerializer
 from mpesa.models import LNMOnline
 
 
-# from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import AllowAny
 
 class LNMCallbackUrlAPIView(CreateAPIView):
     queryset = LNMOnline.objects.all()
     serializer_class = LNMOnlineSerializer
+    permissions_class = [AllowAny]
 
     def create(self, request):
         print(request.data,"This is request.data")
