ce0819ecd5047786a1aef4cf239a3b6623485a13
678jupiter <jupiterhumphrey@gmail.com>
2022-01-28 12:01:37 +0300

    aksjbc

--- cred.md
+++ cred.md
@@ -6,3 +6,27 @@
 
 # python manage.py runserver
 
+
+
+
+        """
+        {'Body':
+            {'stkCallback':
+             {
+                'CheckoutRequestID': 'ws_CO_DMZ_401669274_11032019190235305',
+                'MerchantRequestID': '19927-3244045-1',
+                'ResultCode': 0,
+                'ResultDesc': 'The service request is processed successfully.',
+                'CallbackMetadata': {
+                                        'Item': [
+                                                {'Name': 'Amount', 'Value': 1.0},
+                                                {'Name': 'MpesaReceiptNumber', 'Value': 'NCB1FW1DFZ'},
+                                               
+                                                {'Name': 'TransactionDate', 'Value': 20190311190244},
+                                                {'Name': 'PhoneNumber', 'Value': 254718821114}
+                                                ]
+                                    }
+                                    }
+                }
+        }
+        """
--- mpesa/api/views.py
+++ mpesa/api/views.py
@@ -12,3 +12,58 @@ class LNMCallbackUrlAPIView(CreateAPIView):
 
     def create(self, request):
         print(request.data,"This is request.data")
+
+
+        """ 
+        {'Body': 
+        {'stkCallback': 
+        {
+            'MerchantRequestID': '14831-6336888-1', 
+            'CheckoutRequestID': 
+            'ws_CO_280120221124004074', 
+            'ResultCode': 0, 
+            'ResultDesc': 'The service request is 
+            processed successfully.', 
+        'CallbackMetadata': {
+                              'Item': [
+                                  {'Name': 
+                                  'Amount',
+                                   'Value': 1.0}, 
+                                  {'Name': 
+                                  'MpesaReceiptNumber', 
+                                  'Value': 
+                                  'QAS59KSDSR'}, 
+                                 {'Name': 
+                                 'TransactionDate', 
+                                 'Value': 
+                                 20220128112530}, 
+                                {'Name': 
+                                'PhoneNumber', 
+                                'Value': 
+                                  254799568838}]}}}}
+        
+         """
+
+        merchant_request_id = request.data["Body"]["stkCallback"]["MerchantRequestID"]
+        print(merchant_request_id, "this should be MerchantRequestID")
+        checkout_request_id = request.data["Body"]["stkCallback"]["CheckoutRequestID"]
+        result_code = request.data["Body"]["stkCallback"]["ResultCode"]
+        result_description = request.data["Body"]["stkCallback"]["ResultDesc"]
+        amount = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][0][
+            "Value"
+        ]
+        print(amount, "this should be an amount")
+        mpesa_receipt_number = request.data["Body"]["stkCallback"]["CallbackMetadata"][
+            "Item"
+        ][1]["Value"]
+        print(mpesa_receipt_number, "this should be an mpesa_receipt_number")
+
+        transaction_date = request.data["Body"]["stkCallback"]["CallbackMetadata"][
+            "Item"
+        ][3]["Value"]
+        print(transaction_date, "this should be an transaction_date")
+
+        phone_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][
+            4
+        ]["Value"]
+        print(phone_number, "this should be an phone_number")
