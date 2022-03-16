d7350bc010271ce3ee63a9fa9a7ffeff7b699b00
678jupiter <jupiterhumphrey@gmail.com>
2022-03-16 10:54:50 +0300

    make it better

--- darajapayment/settings/production.py
+++ darajapayment/settings/production.py
@@ -135,7 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-cdn-local")
 
 
 REST_FRAMEWORK = {
