a8f08590c7e5ac57b9e94852414e2099aa2e9a1e
678jupiter <jupiterhumphrey@gmail.com>
2022-03-16 11:12:11 +0300

    hdhdh

--- darajapayment/settings/production.py
+++ darajapayment/settings/production.py
@@ -139,6 +139,8 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-cdn-local")
 
 
 REST_FRAMEWORK = {
